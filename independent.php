4b99ea84205cc063bd528de39090b4dc3850330e
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:07:07 +0100

    Add files via upload

---
+++ independent.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Women Empowerment Website</title>
+	<?php include('includes/header.php'); ?><!--includes header using php-->
+	<link rel="stylesheet" href="css/styles.css" /> <!--linked to stylesheet named style.css-->
+</head>
+<body class="independent"><!--body tag with class name independent-->
+<div class="row"><!--div class for entire pag-->
+	<h2> Quick Tips That'll Help You Get Better at Taking Risks </h2>
+  <div class="column"><!--div class for first column-->
+		<h3>1.Believe in your vision </h3><!--displays heading in  3rd largest size-->
+    <p><img src='images/look1.png' width='300' alt='A picture of empowerment'/>When people are stuck, it means that they are not connected enough to the end vision. If you think about a goal to run a marathon, the more connected you feel to that end vision, the more motivated you’re going to be,” </p>
+	</div>
+  <div class="column"><!--div class for second column-->
+		<h3> 2. Think Small </h3>
+		<p><img src='images/look2.png' width='300' alt='A picture of empowerment'/>Any time you’re about to make a big leap, whether it’s putting together a deck to secure funding or working on an important pitch, the end result can be overwhelming. Setting small, easily achievable goals is one way to jump-start yourself.</p>
+	</div>
+  <div class="column"><!--div class for 3rd column-->
+	<h3>3.Check Perfection at the Door </h3>
+	<p><img src='images/look3.png' width='300' alt='A picture of empowerment'/>Anytime you’re starting something new, the pursuit of perfection can be paralyzing.
+
+“Choose action over perfection,” says 100 Days Without Fear founder Michelle Poler, who speaks all over the world about overcoming fear. “Women are perfectionists, and we have to let go of that desire to be perfect. We’re too afraid to fail ourselves—but when we don’t even try, we fail ourselves even more.”</p>
+	</div>
+  <div class="column"><!--div class for fourth column-->
+		<h3>4.Make Fear your personal force</h3>
+		<p><img src='images/look3.png' width='300' alt='A picture of empowerment'/>No matter what stage you’re in in your career, fear’s going to be omnipresent. But everyone I spoke to agreed that it can be a great motivator.
+
+“My job has been an exercise in flexing those muscles that I don’t usually flex,” Evvie Crowley says of the digital lifestyle publication, The Caret, she co-founded and launched this year. “I have an entrepreneurial drive, but it’s underneath a lot of insecurity. The best way for me to get over my paralyzing self-doubt is to keep pushing to make it a viable brand.”</p>
+	</div>
+</div>
+</body>
+<?php include('includes/footer.php');?><!--includes footer using php-->
