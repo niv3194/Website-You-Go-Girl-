474b875091e9b1823f384823526c60fa481885d0
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:07:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<?php include('includes/header.php');?><!--includes header using php-->
+<body>
+  <div class="page">
+<div class="caption">
+      <h1>YOU GO GIRL </h1><!--displays heading in largest size-->
+      <div class="full-fledge text-center">
+          <a href="reach.php" class="btn btn-one">Contact Us</a><!--includes buttons which links to contact us and sign-up pages-->
+          <a href="register.php" class="btn btn-two">Login/Sign-up</a>
+      </div><!--close of full fledge-->
+  </div><!--close of caption-->
+  </div><!--close of page-->
+</body><!--close of body tag-->
+<?php include('includes/footer.php');?><!--includes footer using php-->
+</html><!--close of html tag-->
