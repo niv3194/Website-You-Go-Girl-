018cdcbbf444fa0149ae87d36b5892d5e0d78a67
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:02:10 +0100

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer><!--footer class to include footer in all pages-->
+  <div class="footer"><!--the class name of the div tag is footer-->
+    <div class="footer-inner"><!--the class name of the div tag is footer-inner-->
+        Copyrights &copy; 2010 - 2018 All rights reserved.
+      </div><!--close of div tag footer inner-->
+    <div class="footer-bottom"><!--div tag with the class name footer-bottom-->
+        <div class="footer-menu-links"><!--div tag with class name of footer menu links-->
+        <a href="index.php" title="Careers">Home</a><!--links to home page-->
+        <a href="reach.php" title="Sitemap">Contact Us</a><!--links to contact us page-->
+      </div><!--close of footer-menu links-->
+    </div><!--close of footer bottom-->
+  </div><!--close of footer class-->
+</footer><!--close of footer tag-->
