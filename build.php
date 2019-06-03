b84998f37956673e890e8e3a3f82fedab8627908
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:06:11 +0100

    Add files via upload

---
+++ build.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Women Empowerment Website</title>
+	<?php include('includes/header.php');?>
+	<link rel="stylesheet" href="css/styles.css" /> <!--linked to stylesheet named style.css-->
+</head>
+<body class="self">
+<div class="header"><!--div tag to display heading-->
+  <h1>Build Your Self-Confidence</h1><!--displays heading in the largest size-->
+</div>
+<div class="row1"><!--div tag for entire page-->
+  <div class="column1 side"><!--div tag for side column-->
+    <h2>Don't Give Up</h2><!--displays heading in second largest size-->
+    <img src="images/did.png" width='250' height='450' alt="A pic"><!--img tag displays image with attributes height and width-->
+  </div>
+  <div class="column1 middle"><!--div class for middle column-->
+    <h2>Inculcate Self-Confidence</h2><!--displays heading in second largest size-->
+		<!--p tag is to display paragraphs-->
+    <p>Two main things contribute to self-confidence: self-efficacy and self-esteem.
+
+We gain a sense of self-efficacy when we see ourselves (and others similar to ourselves) mastering skills and achieving goals that matter in those skill areas. This is the confidence that, if we learn and work hard in a particular area, we'll succeed; and it's this type of confidence that leads people to accept difficult challenges, and persist in the face of setbacks.This overlaps with the idea of self-esteem  , which is a more general sense that we can cope with what's going on in our lives, and that we have a right to be happy. Partly, this comes from a feeling that the people around us approve of us, which we may or may not be able to control. However, it also comes from the sense that we are behaving virtuously, that we're competent at what we do, and that we can compete successfully when we put our minds to it.
+
+Some people believe that self-confidence can be built with affirmations   and positive thinking  . At Mind Tools, we believe that there's some truth in this, but that it's just as important to build self-confidence by setting and achieving goals – thereby building competence. Without this underlying competence, you don't have self-confidence: you have shallow over-confidence, with all of the issues, upset and failure that this brings.</p>
+    <p>So how do you build this sense of balanced self-confidence, founded on a firm appreciation of reality?
+
+The bad news is that there’s no quick fix, or five-minute solution.
+
+The good news is that becoming more confident is readily achievable, just as long as you have the focus and determination to carry things through. And what’s even better is that the things you’ll do to build your self-confidence will also build success – after all, your confidence will come from real, solid achievement. No-one can take this away from you!
+
+So here are our three steps to self-confidence, for which we’ll use the metaphor of a journey: preparing for your journey; setting out; and accelerating towards success.
+</p>
+ <h3>Step 1: Preparing for Your Journey</h3><!--displays heading in 3rd largest size-->
+<p>
+The first step involves getting yourself ready for your journey to self-confidence. You need to take stock of where you are, think about where you want to go, get yourself in the right mindset for your journey, and commit yourself to starting it and staying with it.</p>
+<h3>Step 2: Setting Out</h3>
+<p>
+This is where you start, ever so slowly, moving towards your goal. By doing the right things, and starting with small, easy wins, you’ll put yourself on the path to success – and start building the self-confidence that comes with this.
+<h3>Step 3: Accelerating Towards Success</h3>
+<p>
+By this stage, you’ll feel your self-confidence building. You’ll have completed some of the courses you started in step 2, and you’ll have plenty of success to celebrate!
+
+This is the time to start stretching yourself. Make the goals a bit bigger, and the challenges a bit tougher. Increase the size of your commitment. And extend the skills you’ve proven into new, but closely related arenas.
+</div><!--close of column1-middle-->
+</div><!--close of row1-->
+<?php include('includes/footer.php');?><!--includes footer using php-->
+</body><!--close of body-->
+</html><!--close of html-->
