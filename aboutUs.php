491322096636b6ce6b7f4673eb1cca644a9cfe40
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:05:52 +0100

    Add files via upload

---
+++ aboutUs.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Women Empowerment Website</title>
+	<?php include('includes/header.php'); ?><!--includes header using php-->
+	<link rel="stylesheet" href="css/styles.css" />
+	<!--linked to stylesheet named style.css-->
+</head>
+<body class="mission"><!--body tag with class"mission"-->
+		<div class="about-us">
+			<h1>About You Go Girl</h1><!--displays heading in largest size-->
+			<h2>Our Mission</h2><!--displays heading in second largest size-->
+			<!--p tag s do display a paragraph and img tag displays an image-->
+			<p><img src='images/empower.jpg' width='575' alt='A picture of empowerment'/>Here at You Go Girl, we want you to believe that you have the power to achieve your dreams. It is our sincerest wish — through the sharing of our personal experiences — that we inspire you to pursue your big dream.
+        Even if you are sick, overweight, over age 55, under age 21, married, divorced, rich, without a high school diploma, unemployed, or striving to make your next meal–you can dream big–because many dreamers begin their journey while life is kicking them in the teeth. Each You Go Girl article contributor typically shares a new dream story for a year.Other contributors have offered their dream encouragement for years with no end in sight!</p>
+			</div><!--close of about-us-->
+		</body><!--close of body-->
+				<?php include('includes/footer.php');?><!--includes footer using php-->
+			</html>
