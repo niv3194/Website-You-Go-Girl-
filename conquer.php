b9b0d16648a56e94c2e7977b5c6c85c3490909a1
Nivedha Nagarajan <45610825+niv3194@users.noreply.github.com>
2019-06-03 15:06:31 +0100

    Add files via upload

---
+++ conquer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Women Empowerment Website</title>
+   <?php include('includes/header.php'); ?><!--includes header using php-->
+	<link rel="stylesheet" href="css/styles.css" />
+	 <!--linked to stylesheet named style.css-->
+ </head>
+<body class="conquer"><!--div class for entire page-->
+	 <div class="header1"><!--div class for heading-->
+	   <h2>Women of Worth</h2><!--h2 tag displays the heading in second largest size-->
+	 </div>
+	 <div class="row2"><!--div tag with the class name row2-->
+	   <div class="leftcolumn"><!--div class for next section-->
+	     <div class="card"><!--div tag with the class name card-->
+	       <h2>Helen Keller</h2><!--displays heading in second largest size-->
+	       <div class="fakeimg"><!--div tag with the class name fakeimg-->
+					 <!--img tag adds the image and gives a width to it-->
+				 <img src="images/hk.jpg" width="200" alt="A pic"></div>
+				 <!--p tag displays the paragraph-->
+	       <p>Helen Keller made tremendous progress in learning to communicate due to the exceptional effort of her instructor Anne Sullivan, who evolved into her governess and later her companion. In 1888, Keller entered the Perkins Institute for the Blind in Watertown, Massachusetts. In 1894, she began attending Wright-Humason School for the Deaf in New York City and in 1896, she returned to Massachusetts to enter The Cambridge School for Young Ladies, a preparatory school for women. In 1900, Keller gained admittance to the famous Radcliffe College in Cambridge and in 1904, she graduated with a Bachelor of Arts degree. Helen Keller thus became the first deaf blind person to earn a bachelor’s degree.During her junior year at Radcliffe, Helen Keller wrote her autobiography The Story of My Life, in which she recounted her journey from a child with an extreme handicap to a 21 year old student at Radcliffe. Among other things, the book talks about her education with Anne Sullivan describing how Sullivan enabled her to communicate with the world. The Story of My Life was published in 1903 when Keller was 22 years old. It has since been widely published and is still in print in over fifty languages. It was adapted into the famous Tony award winning play The Miracle Worker by William Gibson which premiered on Broadway in 1959 and ran for 719 performances; and a 1962 Oscar winning Hollywood film also titled The Miracle Worker.
+</p><!--close of p tag-->
+</div><!--close of div tag-->
+	     <div class="card"><!--div tag with the class name card-->
+	       <h2>Ada Lovelace </h2>
+	       <div class="fakeimg"><!--div tag with the class fakeimg-->
+				 <img src="images/computer.jpg"  width="200" alt="A pic"></div>
+	       <p>Lady Byron, did not want her to turn out to be like her father, a romantic poet," says Isaacson. So Lady Byron "had her tutored almost exclusively in mathematics as if that were an antidote to being poetic."
+
+Lovelace saw the poetry in math. At 17, she went to a London salon and met Charles Babbage. He showed her plans for a machine that he believed would be able to do complex mathematical calculations. He asked Lovelace to write about his work for a scholarly journal. In her article, Lovelace expresses a vision for his machine that goes beyond calculations.
+
+She envisioned that "a computer can do anything that can be noted logically," explains Isaacson. "Words, pictures and music, not just numbers. She understands how you take an instruction set and load it into the machine, and she even does an example, which is programming Bernoulli numbers, an incredibly complicated sequence of numbers."</p>
+	     </div>
+	   </div>
+	   <div class="rightcolumn">
+	     <div class="card"><!--div tag with the class name card-->
+	       <h2>J.K Rowling</h2>
+	       <div class="fakeimg" ><!--div tag with the class fakeimg-->
+					 <img src="images/rowling.jpg"  width="200" alt="A pic"></div>
+	       <p>J.K. Rowling had just gotten a divorce, was on government aid, and could barely afford to feed her baby in 1994, just three years before the first Harry Potter book, Harry Potter and The Philosopher’s Stone, was published. When she was shopping it out, she was so poor she couldn’t afford a computer or even the cost of photocopying the 90,000-word novel, so she manually typed out each version to send to publishers. It was rejected dozens of times until finally Bloomsbury, a small London publisher, gave it a second chance after the CEO’s eight year-old daughter fell in love with it.</p>
+	     </div>
+
+	     <div class="card"><!--div tag with the class name card-->
+				 </div>
+	       <h3>Ophrah Winfrey</h3>
+				 <!--div tag with the class fakeimg-->
+	       <div class="fakeimg"><img src="images/winfrey.jpg"  width="200" alt="A pic"></div><br>
+				 <p>Oprah’s dealt with a lot throughout her public life—criticism about her weight, racism, intrusive questions about her sexuality, just to name a few—but she never let it get in the way of her ambition and drive. When you look at her childhood, her personal triumphs are cast in an even more remarkable light.
+
+Growing up, Oprah was reportedly a victim of sexual abuse and was repeatedly molested by her cousin, an uncle, and a family friend. Later, she became pregnant and gave birth to a child at age 14, who passed away just two weeks later. But Oprah persevered, going on to finish high school as an honors student, earning a full scholarship to college, and working her way up through the ranks of television, from a local network anchor in Nashville to an international superstar and creator of her OWN network (we couldn’t help ourselves).</p>
+</div><!--close of rightcolumn-->
+
+</div><!--close of row2-->
+</body><!--close of body-->
+	 <?php include('includes/footer.php');?><!--includes footer using php-->
+	 </html>
